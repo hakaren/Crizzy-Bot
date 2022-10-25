81693008b0cf5f5afce65f07f7a561d8dbc20d55
hakaren <115398537+hakaren@users.noreply.github.com>
2022-10-25 13:49:19 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,18 +10,18 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6282144607416',
+  atasnama : 'aga'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
-global.profilebio = ['6283102650464'] // ini ganti nomor lu ntar bisa nampilin pp lu
+global.owner = ['6282144607416',"6282144607416"]
+global.premium = ['6282144607416']
+global.profilebio = ['6282144607416'] // ini ganti nomor lu ntar bisa nampilin pp lu
 global.sponsor = 'no sponsors in:('
-global.packname = 'Crizzy-Bot'
-global.footer = '© 2022 Fadhil Graphy'
-global.author = 'Owner : Fadhil Graphy\nIg1 : @fdhlgrphy\nIg2 : @nr.fdhl__'
+global.packname = 'jega-Bot'
+global.footer = '© 2022 aga-jega'
+global.author = 'Owner : agaaa'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.act = {
 		wait: 'please wait a moment, processing data...',
